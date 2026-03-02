a9f05d88fc8be17e6913b818bb877655cb3b2139
paramila <paramila@unirioja.es>
2026-03-02 13:05:33 +0100

    base de datos

---
+++ app-peluqueria/prisma/migrations/20260302120459_inicio/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Servicio` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(191) NOT NULL,
+    `descripcion` VARCHAR(191) NULL,
+    `precio` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
